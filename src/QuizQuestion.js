708a23d4cae9361f4162f7b245cbc9091e018979
Udaysingam <udaysingam@Udaysingams-MacBook-Pro.local>
2019-10-15 11:48:54 +0530

    Add error message upon clicking a wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,14 @@ import QuizQuestionButton from "./QuizQuestionButton";
 class QuizQuestion extends Component {
   constructor(props) {
     super(props);
+    this.state = { incorrectAnswer: false };
   }
 
   handleClick(buttonText) {
-    if (buttonText === this.props.quiz_question.answer) {
+    const isCorrectAnswer = buttonText === this.props.quiz_question.answer;
+    this.setState({ incorrectAnswer: !isCorrectAnswer });
+
+    if (isCorrectAnswer) {
       this.props.showNextQuestionHandler();
     }
   }
@@ -15,6 +19,7 @@ class QuizQuestion extends Component {
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer && <p className='error'>Sorry, that's not right</p>}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
